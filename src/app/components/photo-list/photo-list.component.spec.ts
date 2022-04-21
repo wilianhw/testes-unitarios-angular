5672246a9dbee44b74606b4f82ed11b13f2a495a
Wilian Souza <wilian.souza@dtidigital.com.br>
2022-04-21 10:36:24 -0300

    Teste de integração com um serviço implementado.

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,12 +1,16 @@
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 import { HttpClientModule } from '@angular/common/http';
 import { PhotoListModule } from './photo-list.module';
 import { PhotoBoardModule } from './../../shared/components/photo-board/photo-board.module';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { PhotoListComponent } from './photo-list.component';
+import { buildPhotoList } from 'src/app/shared/components/photo-board/test/build-photo-list';
+import { of } from 'rxjs';
 
 describe(PhotoListComponent.name, () => {
   let fixture: ComponentFixture<PhotoListComponent>;
   let component: PhotoListComponent;
+  let service: PhotoBoardService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +18,31 @@ describe(PhotoListComponent.name, () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(PhotoListComponent);
+    service = TestBed.inject(PhotoBoardService);
     component = fixture.componentInstance;
   });
 
   it('Should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`(D) Should display board when data arrives`, () => {
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos').and.returnValue(of(photos));
+    fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(board).withContext('Should display board').not.toBeNull();
+    expect(loader).withContext('Should not display loader').toBeNull();
+  });
+
+  it(`(D) Should display loader while waiting for data`, () => {
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos').and.returnValue(null);
+    fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(board).withContext('Should not display board').toBeNull();
+    expect(loader).withContext('Should display loader').not.toBeNull();
   });
 });
