71d32f20a6a0061907f3b5e518d703d9c25f549f
Wilian Souza <wilian.souza@dtidigital.com.br>
2022-04-21 09:00:42 -0300

    Implementado teste com ngOnChanges.

--- src/app/shared/components/photo-board/photo-board.component-outro.spec.ts
+++ src/app/shared/components/photo-board/photo-board.component-outro.spec.ts
@@ -2,10 +2,16 @@ import { Photo } from './interfaces/photo';
 import { PhotoBoardModule } from './photo-board.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoBoardComponent } from './photo-board.component';
-import { SimpleChange, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  SimpleChange,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
-let fixture: ComponentFixture<PhotoBoardComponent>;
-let component: PhotoBoardComponent;
+let fixture: ComponentFixture<PhotoBoardTestComponent>;
+let component: PhotoBoardTestComponent;
 
 function buildPhotoList(): Photo[] {
   const photos: Photo[] = [];
@@ -23,26 +29,31 @@ function buildPhotoList(): Photo[] {
 describe(PhotoBoardComponent.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      declarations: [PhotoBoardTestComponent],
       imports: [PhotoBoardModule],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(PhotoBoardComponent);
+    fixture = TestBed.createComponent(PhotoBoardTestComponent);
     component = fixture.componentInstance;
   });
 
   it('Should display rows and columns when (@Input photos) has value', () => {
     component.photos = buildPhotoList();
     fixture.detectChanges();
-    const change: SimpleChanges = {
-      photos: new SimpleChange([], component.photos, true),
-    };
-    component.ngOnChanges(change);
-    expect(component.rows.length).withContext('Number of rows').toBe(2);
-    expect(component.rows[0].length)
+    expect(component.board.rows.length).withContext('Number of rows').toBe(2);
+    expect(component.board.rows[0].length)
       .withContext('Number of cols in first row')
       .toBe(4);
-    expect(component.rows[1].length)
+    expect(component.board.rows[1].length)
       .withContext('Number of cols in first row')
       .toBe(4);
   });
 });
+
+@Component({
+  template: ` <app-photo-board [photos]="photos"> </app-photo-board> `,
+})
+class PhotoBoardTestComponent {
+  @ViewChild(PhotoBoardComponent) board: PhotoBoardComponent;
+  photos: Photo[] = [];
+}
